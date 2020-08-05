fb1e8025f36632b693b8d120145f1dff858f13c7
Michelle Macato <michellefmacato@gmail.com>
2020-08-05 18:06:13 +0900

    yelp is 90 done

--- db/migrate/20200805024620_create_restaurants.rb
+++ db/migrate/20200805024620_create_restaurants.rb
@@ -3,10 +3,8 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
     create_table :restaurants do |t|
       t.string :name
       t.string :address
-      t.integer :phone_number
+      t.string :phone_number
       t.string :category
-      t.string :review
-
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+100.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORY.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+puts "Created #{Restaurant.count} restaurants..."
